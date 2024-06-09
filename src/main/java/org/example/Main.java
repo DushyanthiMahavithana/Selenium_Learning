caeac34d917f92b76b5462ab9f963fd8847e7a9c
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2024-06-09 22:05:49 +0530

    first test case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,7 +12,11 @@ public class Main {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         //EdgeDriver driver = new EdgeDriver();
-        driver.get("https://www.ebay.com/");
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+
 
 
     }
